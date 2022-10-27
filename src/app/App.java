cc62eaa8d5422e92723fb82bf39e3e39ee1ef0d9
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-27 19:20:58 -0300

    feat: Requisição no banco

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,20 @@
 package app;
 
+import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class App {
   public static void main(String[] args) {
+    Department obj = new Department(1, "Books");
 
+    SellerDao sellerDao = DaoFactory.createSellerDao();
+
+    Seller seller = sellerDao.findById(3);
+
+    System.out.println(seller);
   }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import database.Database;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
   public static SellerDao createSellerDao() {
-    return new SellerDaoJDBC();
+    return new SellerDaoJDBC(Database.getConnection());
   }
 }
