7fde33c8ae525be814f5641811e67c4a304559ad
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-28 10:50:56 -0300

    feat: Pegar todos os departamentos

--- src/app/App.java
+++ src/app/App.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -11,42 +12,50 @@ import model.entities.Seller;
 public class App {
   public static void main(String[] args) {
 
-    SellerDao sellerDao = DaoFactory.createSellerDao();
+    // SellerDao sellerDao = DaoFactory.createSellerDao();
 
-    System.out.println("==== Buscando Vendedor por Id");
-    Seller seller = sellerDao.findById(3);
+    // System.out.println("==== Buscando Vendedor por Id");
+    // Seller seller = sellerDao.findById(3);
 
-    System.out.println(seller);
+    // System.out.println(seller);
 
-    System.out.println("\n==== Buscando Vendedores pelo Departamento");
-    Department department = new Department(2, null);
-    List<Seller> list = sellerDao.findByDepartament(department);
+    // System.out.println("\n==== Buscando Vendedores pelo Departamento");
+    // Department department = new Department(2, null);
+    // List<Seller> list = sellerDao.findByDepartament(department);
 
-    for (Seller s : list) {
-      System.out.println(s);
+    // for (Seller s : list) {
+    // System.out.println(s);
+    // }
+
+    // System.out.println("\n==== Buscando todos os vendedores");
+    // List<Seller> listAll = sellerDao.findAll();
+
+    // for (Seller s : listAll) {
+    // System.out.println(s);
+    // }
+
+    // System.out.println("\n==== Inserir novo Vendedor");
+    // Seller newSeller = new Seller(null, "Fernando", "email@emailTop.com", new
+    // Date(), 4000d, department);
+    // sellerDao.insert(newSeller);
+    // System.out.println("Inserido! Id do novo vendedor = " + newSeller.getId());
+
+    // System.out.println("\n=== Atualizar Vendedor");
+    // seller = sellerDao.findById(1);
+    // seller.setName("Martha Waine");
+    // sellerDao.update(seller);
+    // System.out.println("Atualizado com sucesso");
+
+    // System.out.println("\n=== Deletar um Vendedor");
+    // sellerDao.deleteById(2);
+    // System.out.println("Deletado com sucesso");
+
+    DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+    System.out.println("\n=== Pegando todos os Departamentos");
+    List<Department> listDepartaments = departmentDao.findAll();
+
+    for (Department department : listDepartaments) {
+      System.out.println(department);
     }
-
-    System.out.println("\n==== Buscando todos os vendedores");
-    List<Seller> listAll = sellerDao.findAll();
-
-    for (Seller s : listAll) {
-      System.out.println(s);
-    }
-
-    System.out.println("\n==== Inserir novo Vendedor");
-    Seller newSeller = new Seller(null, "Fernando", "email@emailTop.com", new Date(), 4000d, department);
-    sellerDao.insert(newSeller);
-    System.out.println("Inserido! Id do novo vendedor = " + newSeller.getId());
-
-    System.out.println("\n=== Atualizar Vendedor");
-    seller = sellerDao.findById(1);
-    seller.setName("Martha Waine");
-    sellerDao.update(seller);
-    System.out.println("Atualizado com sucesso");
-
-    System.out.println("\n=== Deletar um Vendedor");
-    sellerDao.deleteById(2);
-    System.out.println("Deletado com sucesso");
-
   }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,24 +1,52 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import database.Database;
+import database.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
   private Connection conn;
 
+  private Department instantiateDepartment(ResultSet rs) throws SQLException {
+    Department department = new Department();
+    department.setId(rs.getInt("Id"));
+    department.setName(rs.getString("Name"));
+    return department;
+  }
+
   @Override
   public void deleteById(Integer id) {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public List<Department> findAll() {
-    // TODO Auto-generated method stub
-    return null;
+    PreparedStatement st = null;
+    ResultSet rs = null;
+    List<Department> departments = new ArrayList<>();
+
+    try {
+      st = conn.prepareStatement("SELECT * FROM department;");
+      rs = st.executeQuery();
+      while (rs.next()) {
+        departments.add(instantiateDepartment(rs));
+      }
+
+      return departments;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      Database.closeStatement(st);
+      Database.closeResultSet(rs);
+    }
   }
 
   @Override
